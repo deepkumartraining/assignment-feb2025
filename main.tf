6c23c891b2916a6dea864a6c8f0a209d6531292d
deepkumarchaudhary <deepchaudhary@hotmail.com>
2025-02-20 19:40:57 -0500

    updated coded

--- main.tf
+++ main.tf
@@ -8,14 +8,30 @@ resource "google_app_engine_application" "app" {
   location_id = var.region
 }
 
+
+#resource "google_firestore_index" "indexes" {
+#  for_each = { for idx in var.indexes : idx.index_name => idx }
+#
+#  project    = var.project_id
+#  collection = var.kind_name
+
+#  fields {
+#    field_path = each.value.properties[0].property_name
+#    order      = each.value.properties[0].order
+#  }
+#}
+
 resource "google_firestore_index" "indexes" {
-  count = length(var.indexes)
+  for_each = { for idx in var.indexes : idx.index_name => idx }
 
   project    = var.project_id
   collection = var.kind_name
 
-  fields {
-    field_path = var.indexes[count.index].properties[0].property_name
-    order      = var.indexes[count.index].properties[0].order
+  dynamic "fields" {
+    for_each = each.value.properties
+    content {
+      field_path = fields.value.property_name
+      order      = fields.value.order
+    }
   }
 }
